40f0a1c2ed694dd3f7c8b289c8f77e7e8d516634
Massidama <massidama@outlook.it>
2023-11-21 17:38:44 +0100

    aggiunta classi al div square

--- js/script.js
+++ js/script.js
@@ -3,43 +3,39 @@ const container = document.querySelector(".container");
 
 let square;
 
-
 // costruzione ciclo 
 for (let i = 1; i <= 100; i++) {
-
-   
-
-   
 
     // inserisco le condizioni 
     if ( i % 3 === 0 && i % 5 === 0 ) {
         
         // andiamo a creare il div
-        square =  `<div class="square"> ${"FizzBuzz"} </div>` ;
-
-        let fizzBuzz = document.querySelector(".square");
-        fizzBuzz.classList.add("fizzbuzz");
-
-        fizzBuzz.style.backgroundColor = "red";
+        square =  `<div class="square fizzbuzz"> ${"FizzBuzz"} </div>` ;
 
         // andiamo ad aggiungere il div al container 
         container.innerHTML += square;
+
+        console.log("FizzBuzz");
 
     } else if ( i % 5 === 0 ) {
 
         // andiamo a creare il div
-        square =  `<div class="square"> ${"Buzz"} </div>` ;
+        square =  `<div class="square buzz"> ${"Buzz"} </div>` ;
 
         // andiamo ad aggiungere il div al container 
         container.innerHTML += square;
+
+        console.log("Buzz");
 
     } else if ( i % 3 === 0 ) {
 
         // andiamo a creare il div
-        square =  `<div class="square"> ${"Fizz"} </div>` ;
+        square =  `<div class="square fizz"> ${"Fizz"} </div>` ;
 
         // andiamo ad aggiungere il div al container 
         container.innerHTML += square;
+
+        console.log("Fizz");
         
     } else {
          
@@ -48,6 +44,8 @@ for (let i = 1; i <= 100; i++) {
          
         // andiamo ad aggiungere il div al container 
         container.innerHTML += square;
+
+        console.log(i);
     } 
 
 
